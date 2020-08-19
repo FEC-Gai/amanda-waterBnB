e29cd3f5831f81daacf6f95f3db966c85efbacf7
Amanda Winkles <amandawinkles@gmail.com>
2020-08-19 17:43:06 -0500

    getting image objects from unsplash successfully

--- ImagesService/database/helpers/getUnsplash.js
+++ ImagesService/database/helpers/getUnsplash.js
@@ -1,20 +1,25 @@
 const axios = require('axios');
 const Unsplash = require('unsplash-js').default;
 const { toJson } = require('unsplash-js');
-const envVar = require('dotenv').config({ path: '.../.env' });
+const dotenv = require('dotenv'); //problem with this variable- undefined the way it's getting passed //.config({ path: '.../.env' })
+dotenv.config();
+
+//process.env.UNSPLASH_API_KEY
 
 
 let getUnsplashRooms = (query = 'living,indoors,room') => {
   let options = {
     method: 'GET',
-    url: `https://api.unsplash.com/search/photos/?query=${query}&orientation=landscape&client_id=${envVar}`
+    url: `https://api.unsplash.com/search/photos/?query=${query}&orientation=landscape&client_id=${process.env.UNSPLASH_API_KEY}`
   };
 
   return axios(options)
     //.then(toJson)
     .then((response) => {
-      console.log('🔶response.data', response.data); //undefined
-      return response.data;
+      //response.data shows total # of results & total pages
+      //response.data.results is an array of image objects
+      console.log('🔶response.data', response.data.results);
+      return response.data.results;
     })
     .catch((err) => {
       console.log('error getting room images from Unsplash: ', err);
