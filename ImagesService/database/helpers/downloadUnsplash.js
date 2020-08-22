38623ae887d2d5c1000d692905426f5daeeb7f05
Amanda Winkles <amandawinkles@gmail.com>
2020-08-22 17:25:13 -0500

    successfully downloading 3 photo urls to local folder

--- ImagesService/database/helpers/downloadUnsplash.js
+++ ImagesService/database/helpers/downloadUnsplash.js
@@ -1,34 +1,25 @@
-//const fetch = require('node-fetch');
 const fs = require('fs');
 let Promise = require('bluebird');
-//const util = require('util');
 const path = require('path');
 const axios = require('axios');
-//const request = require('request');
 const { getUnsplashRooms } = require('./getUnsplash.js');
 const writeFilePromise = Promise.promisify(fs.writeFile);
 
 async function downloadRooms() {
   return getUnsplashRooms('living,indoors,room')
     .then((response) => {
-      //only getting 30 max
+      //only getting 30 max //getting 3 as test
       //console.log('🇲🇰response length: ', response.length);
       let photoUrls = [];
       for (let i = 0; i < response.length; i++) {
         let photoObj = response[i];
-        //console.log('🇲🇰photoObj: ', photoObj);
         let photoUrl = photoObj.urls.raw + "&w=1057";
         photoUrls.push(photoUrl);
       }
-      //console.log('🏳️‍🌈photoUrls: ', photoUrls);
       return photoUrls;
     })
     .then((photoUrls) => {
-      //don't need to save as photos, just save as photo urls
-      //let pathArr = [];
-      //do mapping func instead of for loop
       let pathArr = photoUrls.map((photoUrl, index) => {
-        //room1.txt, etc
         index = '' + index;
         let filePath = path.resolve(__dirname, './upload/roomPhotos', 'room' + `${index}.txt`);
         return writeFilePromise(filePath, photoUrl, 'utf8')
