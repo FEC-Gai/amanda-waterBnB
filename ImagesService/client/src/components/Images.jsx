c8742712eaefa3e1b9d920239132ac5380187f35
Amanda Winkles <amandawinkles@gmail.com>
2020-08-27 20:06:02 -0500

    setting up for photos to front end

--- ImagesService/client/src/components/Images.jsx
+++ ImagesService/client/src/components/Images.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+//import PhotoGrid from './PhotoGrid.jsx';
 
 class Images extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class Images extends React.Component {
       hasLoaded: false
     }
     this.getPhotosByRoomId = this.getPhotosByRoomId.bind(this);
+    this.getTitleByRoomId = this.getTitleByRoomId.bind(this);
   }
 
   componentDidMount() {
@@ -34,7 +36,33 @@ class Images extends React.Component {
     })
   }
 
+  getTitleByRoomId(roomId) {
+    //will pass in folder on cloudinary api endpoint
+  axios.get(`http://localhost:3001/${roomId}/title`)
+    .then((response) => {
+      console.log('photos: ', response.data);
+      let allPhotos = [];
+      allPhotos.push(response.data[0]);
+      this.setState({
+        photos: allPhotos,
+        hasLoaded: true
+      });
+    })
+    .catch((err) => {
+      console.log('error getting photos: ', err);
+    })
+  }
+
   render() {
+    const { title } = this.state;
+    const { photos } = this.state;
+    const { hasLoaded } = this.state;
+    if (hasLoaded) {
+      return (
+        //<h1>{ title }</h1>
+        //<PhotoGrid photos={ photos } />
+      );
+    }
     return (
       <h1>Unique Glamping Experience</h1>
     );
